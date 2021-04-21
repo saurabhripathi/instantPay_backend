eadcc638717b1a7427efb58f591d4daa3159d00a
saurabh tripathi <saurabh.tripathi@quovantis.com>
2021-04-21 11:35:57 +0530

    fixed typo

--- src/Routes/task.js
+++ src/Routes/task.js
@@ -3,5 +3,5 @@ const task = require('../controllers/task')
 const router = express.Router();
 router.post('/v1/addTask/:userId', task.addTask)
 router.delete('/v1/deleteTask/:taskId', task.deleteTask)
-router.post('/v1/editTask/:taskId', task.editTask)
+router.put('/v1/editTask/:taskId', task.editTask)
 module.exports = router;    
